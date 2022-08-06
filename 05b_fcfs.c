0a4ebe3ef1d517d0f5ca123c7810f335d7e3396f
Akshay Urs <akshayursm@gmail.com>
2022-08-06 18:24:24 +0530

    bugs fix

--- 05b_fcfs.c
+++ 05b_fcfs.c
@@ -43,10 +43,14 @@ void main()
     {
         P[i].TAT = P[i].CT - P[i].AT;
         P[i].WT = P[i].TAT - P[i].BT;
+        total_tat += P[i].TAT;
+        total_wt += P[i].WT;
     }
     printf("\nID\tAT\tBT\tCT\tWT\tTAT\n");
     for (i = 0; i < n; i++)
     {
         printf("%d\t%d\t%d\t%d\t%d\t%d\n", P[i].ID, P[i].AT, P[i].BT, P[i].CT, P[i].WT, P[i].TAT);
     }
+    printf("AVG TAT : %f\n", total_tat / n);
+    printf("AVG WT : %f\n", total_wt / n);
 }
